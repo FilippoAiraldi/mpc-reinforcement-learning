39607a62ac0ab24451608b587c0558204236f21f
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-11-29 10:39:33 +0100

    implemented experience replay

--- src/mpcrl/experience.py
+++ src/mpcrl/experience.py
@@ -1,4 +1,5 @@
-from typing import Deque, Iterable, Optional, TypeVar
+from itertools import chain
+from typing import Deque, Generator, Iterable, Optional, TypeVar, Union
 
 from csnlp.util.funcs import np_random
 
@@ -6,7 +7,7 @@ T = TypeVar("T")
 
 
 class ExperienceReplay(Deque[T]):
-    """Container class for RL traning to save and sample experience transitions. The
+    """Deque-based class for RL traning to save and sample experience transitions. The
     class inherits from `collections.deque`, adding a couple of simple functionalities
     to it for sampling transitions at random from past observed data."""
 
@@ -31,3 +32,41 @@ class ExperienceReplay(Deque[T]):
         """
         super().__init__(iterable, maxlen=maxlen)
         self.np_random, _ = np_random(seed)
+
+    def sample(
+        self, n: Union[int, float], last_n: Union[int, float] = 0
+    ) -> Generator[T, None, None]:
+        """
+        Samples the experience memory and yields the sampled items.
+
+        Parameters
+        n : int or float
+            Size of the sample to draw from memory, either as integer or percentage of
+            the maximum capacity (requires `maxlen != None`).
+        last_n : int or float, optional
+            Size or percentage of the sample (not of the total memory length) dedicated
+            to including the last items added to the memory. By default, `last_n = 0`.
+
+        Returns
+        -------
+        sample : Iterable of T
+            An iterable sample is yielded.
+
+        Raises
+        ------
+        TypeError
+            Raises if `n` is float (a percentage of the maximum length), but `maxlen` is
+            `None`.
+        """
+        L = len(self)
+        if isinstance(n, float):
+            n = int(self.maxlen * n)  # type: ignore
+        n = min(max(n, 0), L)
+        if isinstance(last_n, float):
+            last_n = int(n * last_n)
+        last_n = min(max(last_n, 0), n)
+
+        # get last n indices and the sampled indices from the remaining
+        last = range(L - last_n, L)
+        sampled = self.np_random.choice(range(L - last_n), n - last_n, replace=False)
+        yield from (self[i] for i in chain(last, sampled))
--- tests/test_experience.py
+++ tests/test_experience.py
@@ -1,4 +1,5 @@
 import os
+from itertools import product
 import tempfile
 import unittest
 from typing import Tuple
@@ -42,6 +43,41 @@ class TestExperienceReplay(unittest.TestCase):
             os.remove(f"{TMPFILENAME}.pkl")
         finally:
             return super().tearDown()
+
+    def test_sample__raises__with_no_maxlen_and_percentage_size(self):
+        mem = ExperienceReplay[Tuple[np.ndarray, float]](maxlen=None)
+        with self.assertRaises(TypeError):
+            list(mem.sample(n=0.0))
+
+    def test_sample__with_zero_samples__returns_no_samples(self):
+        mem = ExperienceReplay[Tuple[np.ndarray, float]](maxlen=100)
+        for n in (0, 0.0):
+            self.assertListEqual(list(mem.sample(n)), [])
+
+    def test_sample__returns_right_sample_size(self):
+        N = 100
+        Nsample = 10
+        mem = ExperienceReplay[Tuple[np.ndarray, float]](maxlen=N)
+        mem.extend(range(N))
+        for n in (Nsample, Nsample / N):
+            sample = list(mem.sample(n=n))
+            self.assertEqual(len(sample), Nsample)
+            for item in sample:
+                self.assertIn(item, mem)
+
+    def test_sample__last_n__includes_last_n_items(self):
+        N = 100
+        Nsample = 20
+        Nlast = 10
+        mem = ExperienceReplay[Tuple[np.ndarray, float]](maxlen=N)
+        mem.extend(range(N))
+        for n, last_n in product((Nsample, Nsample / N), (Nlast, Nlast / Nsample)):
+            sample = list(mem.sample(n=n, last_n=last_n))
+            self.assertEqual(len(sample), Nsample)
+            for item in sample:
+                self.assertIn(item, mem)
+            for item in range(N - Nlast, N):
+                self.assertIn(item, sample)
 
 
 if __name__ == "__main__":
