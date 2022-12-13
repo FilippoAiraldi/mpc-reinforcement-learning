03d01f80c6d5386acf5cdc1d4eef720bca83cb67
Filippo Airaldi <filippoairaldi@gmail.com>
2022-12-13 14:26:31 +0100

    new warnings

--- src/mpcrl/__init__.py
+++ src/mpcrl/__init__.py
@@ -1,17 +1,26 @@
 __all__ = [
-    "ExperienceReplay",
-    "MpcSolverError",
-    "UpdateError",
-    "Agent",
     "exploration",
+    "schedulers",
+    "Agent",
+    "LearningAgent",
+    "MpcSolverError",
+    "MpcSolverWarning",
+    "UpdateError",
+    "UpdateWarning",
+    "ExperienceReplay",
     "LearnableParameter",
     "LearnableParametersDict",
-    "schedulers",
 ]
 
 import mpcrl.core.exploration as exploration
 import mpcrl.core.schedulers as schedulers
 from mpcrl.agents.agent import Agent
-from mpcrl.core.errors import MpcSolverError, UpdateError
+from mpcrl.agents.learning_agent import LearningAgent
+from mpcrl.core.errors import (
+    MpcSolverError,
+    MpcSolverWarning,
+    UpdateError,
+    UpdateWarning,
+)
 from mpcrl.core.experience import ExperienceReplay
 from mpcrl.core.parameters import LearnableParameter, LearnableParametersDict
--- src/mpcrl/core/errors.py
+++ src/mpcrl/core/errors.py
@@ -2,5 +2,23 @@ class MpcSolverError(RuntimeError):
     """Exception class for raising errors when MPC solvers fails."""
 
 
+class MpcSolverWarning(RuntimeWarning):
+    """Warning class for raising errors when MPC solvers fails."""
+
+    @classmethod
+    def from_exception(cls, ex: MpcSolverError) -> "MpcSolverWarning":
+        """Creates a warning from the corresponding error."""
+        return cls(*ex.args)
+
+
 class UpdateError(RuntimeError):
     """Exception class for raising errors when RL agent's updates fail."""
+
+
+class UpdateWarning(RuntimeWarning):
+    """Warning class for raising errors when RL agent's updates fail."""
+
+    @classmethod
+    def from_exception(cls, ex: UpdateError) -> "UpdateWarning":
+        """Creates a warning from the corresponding error."""
+        return cls(*ex.args)
