915a0d78874121a9aabd7cf545c6b41ec77f8e74
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-12-20 11:58:25 +0100

    better docstring

--- src/mpcrl/wrappers/record_updates.py
+++ src/mpcrl/wrappers/record_updates.py
@@ -8,12 +8,20 @@ from mpcrl.wrappers.wrapper import LearningWrapper, SymType
 
 
 class RecordUpdates(LearningWrapper[SymType, ExpType]):
+    """Wrapper for recording the history of updates by the learning agent."""
 
-    __slots__ = ("learnable_parameters_history",)
+    __slots__ = ("updates_history",)
 
     def __init__(self, agent: LearningAgent[SymType, ExpType]) -> None:
+        """Instantiates the recorder.
+
+        Parameters
+        ----------
+        agent : LearningAgent[SymType, ExpType]
+            The agent whose updates need recording.
+        """
         super().__init__(agent)
-        self.learnable_parameters_history: Dict[str, List[npt.NDArray[np.double]]] = {
+        self.updates_history: Dict[str, List[npt.NDArray[np.double]]] = {
             p.name: [p.value] for p in agent.learnable_parameters.values()
         }
 
@@ -21,4 +29,4 @@ class RecordUpdates(LearningWrapper[SymType, ExpType]):
         """See `LearningAgent.on_update`."""
         self.agent.on_update()
         for par in self.agent.learnable_parameters.values():
-            self.learnable_parameters_history[par.name].append(par.value)
+            self.updates_history[par.name].append(par.value)
