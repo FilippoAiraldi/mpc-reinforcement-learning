e7a3e696cc895d7440a593b818dc8c9d82a4dfa5
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-01-07 14:18:49 +0100

    added repr and str to update strategy class

--- src/mpcrl/core/update.py
+++ src/mpcrl/core/update.py
@@ -57,3 +57,9 @@ class UpdateStrategy:
     def __next__(self) -> bool:
         """With `__iter__`, makes this class act like an iterator."""
         return next(self.update_cycle)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(frequency={self.frequency},hook={self.hook})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
