1dac8d1461d23f41767675f806bd403a5b6414ac
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-23 18:36:02 +0100

    forgot to add new wrapper to init

--- src/mpcrl/wrappers/envs/__init__.py
+++ src/mpcrl/wrappers/envs/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["MonitorEpisodes"]
+__all__ = ["MonitorEpisodes", "MonitorInfos"]
 
 from mpcrl.wrappers.envs.monitor_episodes import MonitorEpisodes
+from mpcrl.wrappers.envs.monitor_infos import MonitorInfos
