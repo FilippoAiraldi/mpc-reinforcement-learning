2aa7d9bdec3f109c7580a1bdd5e2742d73740551
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-09-13 10:55:39 +0200

    updated examples with csnlp==1.5.7

--- examples/dpg.py
+++ examples/dpg.py
@@ -112,7 +112,7 @@ class LinearMpc(Mpc[cs.SX]):
         B = self.parameter("B", (nx, nu))
 
         # variables (state, action, slack)
-        x, _ = self.state("x", nx)
+        x, _ = self.state("x", nx, remove_bounds_on_initial=True)
         u, _ = self.action("u", nu, lb=a_bnd[0], ub=a_bnd[1])
         s, _, _ = self.variable("s", (nx, N), lb=0)
 
