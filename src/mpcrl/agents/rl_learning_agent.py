fixed big bug in the computation of the parameter's bounds

src/mpcrl/agents/rl_learning_agent.py
@@ -143,15 +143,14 @@ class RlLearningAgent(
         """Internal utility to retrieve the current bounds on the QP solver for an
         update. Only called if the update problem is not unconstrained, i.e., there are
         either some lb or ub, or a maximum percentage update"""
-        lb = self._learnable_pars.lb
-        ub = self._learnable_pars.ub
+        lb = self._learnable_pars.lb - theta
+        ub = self._learnable_pars.ub - theta
         perc = self.max_percentage_update
-        if perc == float("+inf"):
-            return lb, ub
-        max_update_delta = np.maximum(np.abs(perc * theta), eps)
-        lbx = np.maximum(lb - theta, -max_update_delta)
-        ubx = np.minimum(ub - theta, +max_update_delta)
-        return lbx, ubx
+        if perc != float("+inf"):
+            max_update_delta = np.maximum(np.abs(perc * theta), eps)
+            lb = np.maximum(lb, -max_update_delta)
+            ub = np.minimum(ub, +max_update_delta)
+        return lb, ub
 
     def _do_gradient_update(
         self, g: npt.NDArray[np.floating], H: Optional[npt.NDArray[np.floating]]

tests/test_quadrotor_q_learning.py
@@ -993,8 +993,8 @@ class QuadRotorBaseLearningAgent(QuadRotorBaseAgent, ABC):
         max_perc_update: float,
     ) -> tuple[np.ndarray, np.ndarray]:
         max_delta = np.maximum(np.abs(max_perc_update * theta), 0.1)
-        lb = np.maximum(theta_bounds[:, 0], theta - max_delta)
-        ub = np.minimum(theta_bounds[:, 1], theta + max_delta)
+        lb = np.maximum(theta_bounds[:, 0], theta - max_delta) - theta
+        ub = np.minimum(theta_bounds[:, 1], theta + max_delta) - theta
         return lb, ub
 
 